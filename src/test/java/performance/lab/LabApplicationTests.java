9916d30e0284820eeca94515cf3f9237dabd07bf
yybmion <yyb400@naver.com>
2025-02-17 00:06:28 +0900

    백엔드 초기설정

---
+++ src/test/java/performance/lab/LabApplicationTests.java
@@ -0,0 +1,13 @@
+package performance.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
